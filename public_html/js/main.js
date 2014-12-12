24c13c0bca26dd6c9e34eb8e747d0d3e597e041c
Christian Hansen <christian@herrhansen.com>
2014-12-12 13:32:46 +0100

    schreibe befehl und ausgabe in Terminal

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,41 @@
+(function($) {
+    $.fn.writeText = function(content, callback) {
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+        interval(function() {
+            if(current < contentArray.length) {
+                elem.text(elem.text() + contentArray[current++]);
+            } else {
+                if (typeof callback === 'function') { // make sure the callback is a function
+                    callback.call(this); // brings the scope to the callback
+                } 
+            }
+        }, 250, 10);
+    };
 
+})(jQuery);
+
+function interval(func, wait, times){
+    var interv = function(w, t){
+        return function(){
+            if(typeof t === "undefined" || t-- > 0){
+                setTimeout(interv, w);
+                try{
+                    func.call(null);
+                }
+                catch(e){
+                    t = 0;
+                    throw e.toString();
+                }
+            }
+        };
+    }(wait, times);
+    setTimeout(interv, wait);
+};
+
+$(document).ready(function(){
+   $('#first-input').writeText('whoami', function(){
+       $('.terminal-answer').show();
+   });
+});
